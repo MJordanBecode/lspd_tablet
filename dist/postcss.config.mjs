21360dad7e98a5dedaa0f4c72690815ccc8b962e
Masy Jordan <masyjordan@gmail.com>
2024-12-30 20:44:04 +0100

    supp projet

--- dist/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},  // Active le plugin Tailwind CSS
-    autoprefixer: {}, // Ajoute le plugin Autoprefixer pour la compatibilité des navigateurs
-  },
-};
-
-export default config;
--- server.lua
+++
@@ -1,7 +0,0 @@
-local wantedList = {}
-
-RegisterServerEvent("lspd:updateWantedList")
-AddEventHandler("lspd:updateWantedList", function(newList)
-    wantedList = newList
-    TriggerClientEvent("lspd:syncWantedList", -1, wantedList)
-end)
